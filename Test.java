d779fc415d3c56a5d25aba3cc709404a3c623fbc
a-emane <115736046+a-emane@users.noreply.github.com>
2022-12-02 14:01:40 +0100

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test
+{
+    public static void main(String args[])
+    {
+        System.out.println("PIPELINE AVEC JENKINS");
+        System.out.println("Ceci est un test");
+    }
+}
